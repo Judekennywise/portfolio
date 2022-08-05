f6769a9a57500ad499aff9f58efdcff597aba1da
Jude Oyedele <judeokennywise@gmail.com>
2022-08-05 22:53:54 +0100

    edit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('project/', include('projects.urls')),
+    path('', include('projects.urls')),
 ]
