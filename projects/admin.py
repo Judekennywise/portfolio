cefa2ba75e0c9f0ad8ef5bc8b6a0210abc05ca2a
Jude Oyedele <judeokennywise@gmail.com>
2022-08-06 02:14:15 +0100

    edit

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from projects.models import Project
 # Register your models here.
-class ProjectAdmin():
+class ProjectAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
