9d5bebf4575c5a666b6661f375be4f17c95aaa2d
Jude Oyedele <judeokennywise@gmail.com>
2022-08-06 01:03:57 +0100

    edit

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.project_index, name="project_index"),
+    path("projects/", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
+    path("", views.index, name="index")
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from projects.models import Project
 
+def index(request):
+    return render(request, 'index.html')
+
 def project_index(request):
     projects = Project.objects.all()
     context = {
